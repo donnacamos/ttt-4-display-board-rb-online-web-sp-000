18ae8c9c7af9a1d395a81c598a0ed485d7f72fe6
Donna Amos <donnacamos.developer@gmail.com>
2019-02-13 20:08:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+ 
+  
+
+puts "   |   |   "
+puts "-----------"
+puts "   | #{board[1]} |   "
+puts "-----------"
+puts "   |   |   "
+
+
+
+end
+
